d66cc5c97664a85ef109723e9aa41da94ee48dea
rajaborntohack <digisailordev@gmail.com>
2020-04-05 13:13:54 +0530

    k

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,14 +2,14 @@
 	"use strict";
 	jQuery(document).on('ready', function () {
 
-		// Mean Menu
+        // Mean Menu
 		jQuery('.mean-menu').meanmenu({
 			meanScreenWidth: "991"
         });
 
         // Header Sticky
 		$(window).on('scroll',function() {
-            if ($(this).scrollTop() > 120){
+            if ($(this).scrollTop() > 120){  
                 $('.navbar-area').addClass("is-sticky");
             }
             else{
@@ -29,6 +29,35 @@
             $('.close-btn').addClass('active');
         });
 
+        // Partner Slides
+		$('.partner-slides').owlCarousel({
+			loop: true,
+			nav: false,
+			dots: false,
+			autoplayHoverPause: true,
+            autoplay: true,
+            mouseDrag: true,
+            margin: 30,
+            navText: [
+                "<i class='flaticon-arrow-pointing-to-left'></i>",
+                "<i class='flaticon-arrow-pointing-to-right'></i>"
+            ],
+			responsive: {
+                0: {
+                    items: 2,
+                },
+                576: {
+                    items: 3,
+                },
+                768: {
+                    items: 4,
+                },
+                1200: {
+                    items: 6,
+                },
+            }
+        });
+        
         // Popup Video
 		$('.popup-youtube').magnificPopup({
 			disableOn: 320,
@@ -38,6 +67,136 @@
 			preloader: false,
 			fixedContentPos: false
         });
+
+        // Feedback Slides
+		$('.feedback-slides').owlCarousel({
+			loop: true,
+			nav: false,
+			dots: false,
+			autoplayHoverPause: true,
+            autoplay: true,
+            mouseDrag: true,
+            margin: 30,
+            navText: [
+                "<i class='flaticon-arrow-pointing-to-left'></i>",
+                "<i class='flaticon-arrow-pointing-to-right'></i>"
+            ],
+			responsive: {
+                0: {
+                    items: 1,
+                },
+                576: {
+                    items: 2,
+                },
+                768: {
+                    items: 2,
+                },
+                1200: {
+                    items: 3,
+                },
+            }
+        });
+
+        // FAQ Accordion
+        $(function() {
+            $('.accordion').find('.accordion-title').on('click', function(){
+                // Adds Active Class
+                $(this).toggleClass('active');
+                // Expand or Collapse This Panel
+                $(this).next().slideToggle('fast');
+                // Hide The Other Panels
+                $('.accordion-content').not($(this).next()).slideUp('fast');
+                // Removes Active Class From Other Titles
+                $('.accordion-title').not($(this)).removeClass('active');		
+            });
+        });
+
+        // Progress Bar JS
+        $('.circlechart').circlechart();
+        
+        // Nice Select JS
+        $('select').niceSelect();
+
+        // Subscribe form
+		$(".newsletter-form").validator().on("submit", function (event) {
+			if (event.isDefaultPrevented()) {
+			// handle the invalid form...
+				formErrorSub();
+				submitMSGSub(false, "Please enter your email address.");
+			} else {
+				// everything looks good!
+				event.preventDefault();
+			}
+		});
+		function callbackFunction (resp) {
+			if (resp.result === "success") {
+				formSuccessSub();
+			}
+			else {
+				formErrorSub();
+			}
+		}
+		function formSuccessSub(){
+			$(".newsletter-form")[0].reset();
+			submitMSGSub(true, "Thank you for subscribing!");
+			setTimeout(function() {
+				$("#validator-newsletter").addClass('hide');
+			}, 4000)
+		}
+		function formErrorSub(){
+			$(".newsletter-form").addClass("animated shake");
+			setTimeout(function() {
+				$(".newsletter-form").removeClass("animated shake");
+			}, 1000)
+		}
+		function submitMSGSub(valid, msg){
+			if(valid){
+				var msgClasses = "validation-success";
+			} else {
+				var msgClasses = "validation-danger";
+			}
+			$("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
+		}
+		// AJAX MailChimp
+		$(".newsletter-form").ajaxChimp({
+			url: "https://envytheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
+			callback: callbackFunction
+        });
+
+        // Count Time 
+        function makeTimer() {
+            var endTime = new Date("September 30, 2020 17:00:00 PDT");			
+            var endTime = (Date.parse(endTime)) / 1000;
+            var now = new Date();
+            var now = (Date.parse(now) / 1000);
+            var timeLeft = endTime - now;
+            var days = Math.floor(timeLeft / 86400); 
+            var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+            var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+            var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+            if (hours < "10") { hours = "0" + hours; }
+            if (minutes < "10") { minutes = "0" + minutes; }
+            if (seconds < "10") { seconds = "0" + seconds; }
+            $("#days").html(days + "<span>Days</span>");
+            $("#hours").html(hours + "<span>Hours</span>");
+            $("#minutes").html(minutes + "<span>Minutes</span>");
+            $("#seconds").html(seconds + "<span>Seconds</span>");
+        }
+        setInterval(function() { makeTimer(); }, 1000);
+
+        // Tabs
+        (function ($) {
+            $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+            $('.tab ul.tabs li a').on('click', function (g) {
+                var tab = $(this).closest('.tab'), 
+                index = $(this).closest('li').index();
+                tab.find('ul.tabs > li').removeClass('current');
+                $(this).closest('li').addClass('current');
+                tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+                tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+                g.preventDefault();
+            });
+        })(jQuery);
 
         // Input Plus & Minus Number JS
         $('.input-counter').each(function() {
@@ -69,91 +228,8 @@
                 spinner.find("input").trigger("change");
             });
         });
-		
-		// Feedback Slides
-		$('.feedback-slides').owlCarousel({
-			loop: true,
-			nav: true,
-			dots: true,
-			autoHeight: true,
-			autoplayHoverPause: true,
-            autoplay: true,
-            mouseDrag: true,
-			animateOut: 'fadeOut',
-			items: 1,
-            navText: [
-                "<i class='flaticon-left-chevron'></i>",
-                "<i class='flaticon-right-chevron'></i>"
-            ],
-        });
-
-        // Tabs
-        (function ($) {
-            $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
-            $('.tab ul.tabs li a').on('click', function (g) {
-                var tab = $(this).closest('.tab'), 
-                index = $(this).closest('li').index();
-                tab.find('ul.tabs > li').removeClass('current');
-                $(this).closest('li').addClass('current');
-                tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
-                tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
-                g.preventDefault();
-            });
-        })(jQuery);
         
-        // Nice Select JS
-        $('select').niceSelect();
-		
-		// Tabs
-        (function ($) {
-            $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
-            $('.tab ul.tabs li a').on('click', function (g) {
-                var tab = $(this).closest('.tab'), 
-                index = $(this).closest('li').index();
-                tab.find('ul.tabs > li').removeClass('current');
-                $(this).closest('li').addClass('current');
-                tab.find('.tab-content').find('div.tabs-item').not('div.tabs-item:eq(' + index + ')').slideUp();
-                tab.find('.tab-content').find('div.tabs-item:eq(' + index + ')').slideDown();
-                g.preventDefault();
-            });
-        })(jQuery);
-        
-        // Count Time 
-        function makeTimer() {
-            var endTime = new Date("September 30, 2020 17:00:00 PDT");			
-            var endTime = (Date.parse(endTime)) / 1000;
-            var now = new Date();
-            var now = (Date.parse(now) / 1000);
-            var timeLeft = endTime - now;
-            var days = Math.floor(timeLeft / 86400); 
-            var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-            var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-            var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-            if (hours < "10") { hours = "0" + hours; }
-            if (minutes < "10") { minutes = "0" + minutes; }
-            if (seconds < "10") { seconds = "0" + seconds; }
-            $("#days").html(days + "<span>Days</span>");
-            $("#hours").html(hours + "<span>Hours</span>");
-            $("#minutes").html(minutes + "<span>Minutes</span>");
-            $("#seconds").html(seconds + "<span>Seconds</span>");
-        }
-        setInterval(function() { makeTimer(); }, 1000);
-
-        // FAQ Accordion
-        $(function() {
-            $('.accordion').find('.accordion-title').on('click', function(){
-                // Adds Active Class
-                $(this).toggleClass('active');
-                // Expand or Collapse This Panel
-                $(this).next().slideToggle('fast');
-                // Hide The Other Panels
-                $('.accordion-content').not($(this).next()).slideUp('fast');
-                // Removes Active Class From Other Titles
-                $('.accordion-title').not($(this)).removeClass('active');		
-            });
-        });
-		
-		// Go to Top
+        // Go to Top
         $(function(){
             // Scroll Event
             $(window).on('scroll', function(){
@@ -167,6 +243,20 @@
             });
         });
         
+    });
+
+    // WOW JS
+	$(window).on ('load', function (){
+        if ($(".wow").length) { 
+            var wow = new WOW({
+            boxClass:     'wow',      // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset:       20,          // distance to the element when triggering the animation (default is 0)
+            mobile:       true, // trigger animations on mobile devices (default is true)
+            live:         true,       // act on asynchronously loaded content (default is true)
+          });
+          wow.init();
+        }
     });
     
     // Preloader Area
